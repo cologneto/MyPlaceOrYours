7e5cc25c9b3585578cf10368cdf19d45f9f8ed20
Georgi Tsanev <cologneto@gmail.com>
2017-01-29 13:37:57 +0200

    Last upload Bulgaria

--- js/scripts.js
+++ js/scripts.js
@@ -185,6 +185,15 @@ var jsonObj = [
   }
 ];  
 
+var sectionP = $("section"),
+    windowHeight = $(window).outerHeight(true);
+var scrollEventCounter = 0;
+var lastScrollTop = 0;
+var photoGirlImg = $(".photoGirl").children();
+
+// .first().offset().top;
+
+
 function ShowHideMenuNavigation(x) {
     x.classList.toggle("change");
     $(".navMenuContainer").toggle("hidden");
@@ -207,10 +216,130 @@ $(document).ready(function() {
 
   });
 
-	
+  $("#homeButton").on("click", function() {
+    // body...
+     $("body, html").animate({ 
+      scrollTop: 0 
+    }, 1500);
+  })
+  });
+
+var isDownScroll = function(offsetTop) {
+  if (offsetTop > lastScrollTop){
+    return true;
+  } else {    
+    return false;
+  }
+}
+
+var sectionNumber = function (currPosition) {
+  if ((currPosition >= 0) && (currPosition < windowHeight)) {
+    
+    return 1;
+  } else if ((currPosition >= windowHeight) && (currPosition <= 2*windowHeight)) {
+    return 2;
+  }
+  else if ((currPosition >= 2*windowHeight) && (currPosition <= 3*windowHeight)) {
+    return 3;
+  }
+  else {
+    return 4;
+  }
+}
 
 
+
+$(document).on("scroll", function(e){
+  var offsetTop = $(window).scrollTop();
+  var sectionNumberNow = sectionNumber(offsetTop);
+
+  if ((isDownScroll(offsetTop)) &&  sectionNumberNow == 1) {
+      $(".photoGirl").children().addClass("photoAnimation");
+      $(".map").children().addClass("rotating");
+  }
+
+   if ((isDownScroll(offsetTop)) &&  sectionNumberNow == 3) {
+      $(".apartmentOne").addClass("apartAnimation1");
+      $(".apartmentTwo").addClass("apartAnimation2");
+      $(".apartmentThree").addClass("apartAnimation3");
+     
+  }
 });
+
+$(".stepIn").on("click", function() {
+  var local = window.location.href;
+  window.location.href = local + "apartment1.html"
+})
+
+
+  // $(document).on("scroll", function(e) {
+  //   e.preventDefault();
+  //   
+    
+  //   scrollEventCounter++;
+
+  //   // console.log(isDownScroll(offsetTop));
+  //   // console.log(sectionNumber(offsetTop));
+  //   var sectionNumberNow = sectionNumber(offsetTop);
+  //   if ((isDownScroll(offsetTop)) &&  sectionNumberNow == 1 && scrollEventCounter>=5) {
+
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[1].offsetTop 
+  //       }, 600);
+  //   }
+  //   else if ((isDownScroll(offsetTop)) &&  sectionNumberNow == 2 && scrollEventCounter>=5) {
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[2].offsetTop 
+  //       }, 600);
+  //   }
+  //   else if ((isDownScroll(offsetTop)) &&  sectionNumberNow == 3 && scrollEventCounter>=5) {
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[3].offsetTop 
+  //       }, 600);
+  //   }
+  //   else if ((!isDownScroll(offsetTop)) &&  sectionNumberNow == 4 && scrollEventCounter>=5) {
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[2].offsetTop 
+  //       }, 600);
+  //   }
+  //   else if ((!isDownScroll(offsetTop)) &&  sectionNumberNow == 3 && scrollEventCounter>=5) {
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[1].offsetTop 
+  //       }, 600);
+  //   }
+  //   else if ((!isDownScroll(offsetTop)) &&  sectionNumberNow == 2 && scrollEventCounter>=5) {
+  //       $("body, html").animate({ 
+  //         scrollTop: sectionP[0].offsetTop 
+  //       }, 600);
+  //   }
+  //   // $(window).scrollTop(windowHeight*2);
+  //   lastScrollTop = offsetTop;
+  //   console.log(scrollEventCounter);
+  //   scrollEventCounter == 0;
+
+  // });
+
+
+
+
+
+// var page = $("html, body");
+
+// $( "section" ).click(function(e) {
+
+//    page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+//        page.stop();
+//    });
+
+//    page.animate({ scrollTop: $(this).position().top }, 'slow', function(){
+//        page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+//    });
+
+//    return false; 
+// });
+
+
+
 
 function initMap() {
 	  	
